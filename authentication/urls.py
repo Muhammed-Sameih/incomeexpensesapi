3734d2f129d828e84bd38038da5da014d65de5db
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2023-03-07 13:21:26 +0200

    update url authentication file

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,20 +1,31 @@
-from .views import RegisterAPIView, EmailVerificationAPIView, VerificationEmailRequestAPIView, LoginAPIView, PasswordTokenCheckAPIView, ResetPasswordRequestEmailAPIView, SetNewpasswordAPIView, LogoutAPIView
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    PasswordTokenCheckAPIView,
+    RegisterAPIView,
+    LoginAPIView,
+    LogoutAPIView,
+    RequestPasswordResetEmailAPIView,
+    SetNewPasswordAPIView,
+    VerifyEmailAPIView,
+    RequestEmailVerificationAPIView
+)
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('emailverification/', EmailVerificationAPIView.as_view(),
-         name='emailverification'),
-    path('request-verification-email/',
-         VerificationEmailRequestAPIView.as_view(), name='request-verification-email'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('request-email-verification/', RequestEmailVerificationAPIView.as_view(),
+         name='request-email-verification'),
+    path('email-verify/', VerifyEmailAPIView.as_view(), name="email-verify"),
+    path('request-reset-email/', RequestPasswordResetEmailAPIView.as_view(),
+         name="request-reset-email"),
     path('password-reset/<uidb64>/<token>/',
-         PasswordTokenCheckAPIView.as_view(), name='pasword-reset-confirm'),
-    path('request-password-reset-email/', ResetPasswordRequestEmailAPIView.as_view(),
-         name='request-password-reset-email'),
-    path('set-new-password/', SetNewpasswordAPIView.as_view(),
-         name='set-new-password'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+         PasswordTokenCheckAPIView.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete'),
 ]
