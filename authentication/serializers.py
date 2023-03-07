fc9bade1ce8ecef0f07eac8f7a0ff2f5c8f78fec
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2023-03-07 13:17:45 +0200

    update reset password serializers

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -116,34 +116,44 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
         fields = ['token']
 
 
-class ResetPasswordRequestSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length=80)
+class ResetPasswordEmailRequestSerializer(serializers.Serializer):
+    """Request email for reset password serializer class"""
+    email = serializers.EmailField(min_length=2)
+
+    redirect_url = serializers.CharField(max_length=500, required=False)
 
     class Meta:
         fields = ['email']
-        model = User
 
 
-class SetNewPasswordSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(min_length=8, write_only=True)
-    token = serializers.CharField(min_length=1, write_only=True)
-    uidb64 = serializers.CharField(min_length=1, write_only=True)
+class SetNewPasswordSerializer(serializers.Serializer):
+    """Set new password serializer class"""
+    password = serializers.CharField(
+        min_length=6, max_length=68, write_only=True)
+    token = serializers.CharField(
+        min_length=1, write_only=True)
+    uidb64 = serializers.CharField(
+        min_length=1, write_only=True)
 
     class Meta:
         fields = ['password', 'token', 'uidb64']
-        model = User
 
     def validate(self, attrs):
+        """Validation method for check reset password token for user & add the new password if reset link is valid"""
         try:
             password = attrs.get('password')
-            uidb64 = attrs.get('uidb64')
             token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
             id = force_str(urlsafe_base64_decode(uidb64))
             user = User.objects.get(id=id)
             if not PasswordResetTokenGenerator().check_token(user, token):
-                raise AuthenticationFailed('reset link is invalid', 401)
+                raise AuthenticationFailed('The reset link is invalid', 401)
+
             user.set_password(password)
             user.save()
+
             return (user)
-        except DjangoUnicodeDecodeError:
+        except Exception as e:
             raise AuthenticationFailed('The reset link is invalid', 401)
+        return super().validate(attrs)
