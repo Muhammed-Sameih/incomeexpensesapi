2c01aa45d14ef0b3342896f679eeed82696ab97b
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2023-03-07 13:22:14 +0200

    update utils authentication file

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,9 +1,27 @@
+from django.http import HttpResponsePermanentRedirect
 from django.core.mail import EmailMessage
+import os
+import threading
 
 
 class Util:
     @staticmethod
     def send_email(data):
         email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['email_to']])
+            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
         email.send()
+
+
+class EmailThread(threading.Thread):
+
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+
+class CustomRedirect(HttpResponsePermanentRedirect):
+
+    allowed_schemes = [os.environ.get('APP_SCHEME'), 'http', 'https']
