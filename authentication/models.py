ad8d9fb0ff4e5f220ea67ea288f81114766a39c6
Mu7amed Samei7 <muhamedsamih748@gmail.com>
2023-03-07 12:43:04 +0200

    update user model

--- authentication/models.py
+++ authentication/models.py
@@ -1,66 +1,64 @@
 from django.db import models
-from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from rest_framework_simplejwt.tokens import RefreshToken
-# Create your models here.
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, mobile=None, password=None):
+    def create_user(self, username, email, mobile, password=None):
+        """Method for create new user with based fields"""
         if username is None:
             raise TypeError('user name must exists!')
         if email is None:
-            raise TypeError('email must exists!')
+            raise TypeError('Email address is required')
+
         user = self.model(
-            username=username,
             email=self.normalize_email(email),
+            username=username,
             mobile=mobile
         )
         user.set_password(password)
         user.save()
+
         return user
 
-    def create_superuser(self, username, email, password, mobile=None):
+    def create_superuser(self, email, username, password=None):
         if password is None:
-            raise TypeError('password must exists!')
+            raise TypeError('Password must not be None')
+
         user = self.create_user(
-            username=username, email=email, mobile=mobile, password=password)
-        user.is_staff = True
+            username=username, mobile=None, email=email, password=password)
         user.is_superuser = True
-        user.is_verified = True
         user.is_admin = True
+        user.is_staff = True
+        user.is_verified = True
         user.save()
+
         return user
-
-
-AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
-                  'twitter': 'twitter', 'email': 'email'}
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=50)
-    mobile = models.CharField(
-        max_length=11, unique=True, null=True, blank=True, default=None)
-    email = models.EmailField(max_length=80, unique=True)
+    """User class with email field used for login & custom fields like [username, mobile, password]"""
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    username = models.CharField(max_length=50, default="admin")
+    mobile = models.CharField(max_length=11, null=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=True)
-    is_verified = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    auth_provider = models.CharField(
-        max_length=255, blank=False,
-        null=False, default=AUTH_PROVIDERS.get('email'))
 
-    objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    objects = UserManager()
+
     def __str__(self):
-        return self.username
+        return self.email
 
     def get_tokens(self):
+        "Method to get tokens for user"
         refresh = RefreshToken.for_user(self)
         return {
             'refresh': str(refresh),
